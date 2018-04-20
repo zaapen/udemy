1f8a0f49df4918022b410b88cfd3ff00354826a5
Danny <zaapen@gmail.com>
2018-04-20 00:08:46 -0700

    updated section7

--- udemy_javascript/section7/7-EcmaScript2015/script.js
+++ udemy_javascript/section7/7-EcmaScript2015/script.js
@@ -335,11 +335,82 @@ Array.from(all).forEach(cur => cur.style.color = 'purple'); //converts to array
 ////////////////////////////////////////////////////////
 // Lecture: Rest Parameters
 
+/*
+// ES5
+
+function isFullAge5() {
+	// console.log(arguments);
+	var argsArr = Array.prototype.slice.call(arguments); // Turns into array
+
+	argsArr.forEach(function(cur) {
+		console.log((2016 - cur) >= 18);
+	});
+}
+
+isFullAge5(1990, 1999, 1965);
+// isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+// ES6
+function isFullAge6(...years) {
+	years.forEach(cur => console.log((2016 - cur) >= 18));
+}
+isFullAge6(1990, 1999, 1965, 2016, 1987);
+
+// ES5
+
+function isFullAge5(limit) {
+	// console.log(arguments);
+	var argsArr = Array.prototype.slice.call(arguments, 1); // Ignores the first argument(limit) and turns into array
+
+	// console.log(argsArr);
+	argsArr.forEach(function(cur) {
+		console.log((2016 - cur) >= limit);
+	});
+}
+
+// isFullAge5(16, 1990, 1999, 1965);
+// isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+// ES6
+function isFullAge6(limit, ...years) {
+	years.forEach(cur => console.log((2016 - cur) >= limit));
+}
+isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
+
+*/
+
+////////////////////////////////////////////////////////////////////////////////////
+// Lecture: Default parameters
+/*
+// ES5
+function SmithPerson(firstName, yearOfBirth, lastName, nationality) {
+
+	lastName === undefined ? lastName = 'Smith' : lastName = lastName;
+	nationality === undefined ? nationality = 'american' : nationality = nationality;
+
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.yearOfBirth = yearOfBirth;
+	this.nationality = nationality;
+}
 
 
 
+// ES6
+function SmithPerson(firstName, yearOfBirth, lastName = 'Smith', nationality = 'american') {
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.yearOfBirth = yearOfBirth;
+	this.nationality = nationality;
+}
 
+var john = new SmithPerson('John', 1990);
+var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish');
 
+*/
+
+///////////////////////////////////////////////////////////////////////////////////
+// Lecture:
 
 
 
