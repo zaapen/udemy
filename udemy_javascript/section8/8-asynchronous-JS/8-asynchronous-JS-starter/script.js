4462f6a22f4f5eedbe1a77a8bd6f9066488c1b78
Danny Hua <dannyhua@DongleBook-Pro.socal.rr.com>
2018-04-22 21:59:32 -0700

    updated section8

--- udemy_javascript/section8/8-asynchronous-JS/8-asynchronous-JS-starter/script.js
+++ udemy_javascript/section8/8-asynchronous-JS/8-asynchronous-JS-starter/script.js
@@ -1,3 +1,4 @@
+/*
 function getRecipe() {
     setTimeout(() => {
         const recipeID = [523, 883, 432, 974];
@@ -21,3 +22,49 @@ function getRecipe() {
     }, 1500);
 }
 getRecipe();
+*/
+
+const getIDs = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve([523, 883, 432, 974]);
+    }, 1500);
+});
+
+const getRecipe = recID => {
+    return new Promise((resolve, reject) => {
+        setTimeout(ID => {
+            const recipe = {
+                title: 'Fresh tomato pasta',
+                publisher: 'Jonas'
+            };
+            resolve(`${ID}: ${recipe.title}`);
+        }, 1500, recID)
+    });
+};
+
+const getRelated = publisher => {
+    return new Promise((resolve, reject) => {
+        setTimeout(pub => {
+            const recipe = {
+                title: 'Italian Pizza',
+                publisher: 'Jonas'
+            };
+            resolve(`${pub}: ${recipe.title}`);
+        }, 1500, publisher)
+    });
+};
+
+getIDs.then(IDs => {
+        console.log(IDs);
+        return getRecipe(IDs[2]);
+    })
+    .then(recipe => {
+        console.log(recipe);
+        return getRelated('Jonas')
+    })
+    .then(recipe => {
+        console.log(recipe);
+    })
+    .catch(error => {
+        console.log('Error!')
+    })
